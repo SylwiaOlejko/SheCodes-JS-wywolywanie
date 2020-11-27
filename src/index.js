5880097b819ce47ab0eda604d3196b39b181a463
SylwiaOlejko <63730148+SylwiaOlejko@users.noreply.github.com>
2020-11-27 15:47:13 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// Challenge
+// 1. Create a function isWindy
+// 2. Add a speed parameter to the function
+function isWindy(speed){
+ 
+// 3. If speed is greater than 5, alert ‘It is windy’
+if(speed > 5){
+  alert('It is windy!');
+// 4. Else, alert ‘It is not windy’
+  }else{
+    alert('It is not windy!');
+  }
+}
+// 5. Call the function twice with different arguments to test both scenarios
+ let speed = 2;
+isWindy(1);
+isWindy(20);
+isWindy(5);
+isWindy(200);
+
+
+
+
+
+
